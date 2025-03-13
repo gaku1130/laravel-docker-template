c2ef5b00dd76338937b7cecb151fdff1ebe05126
gaku1130 <gk.99prd3o@gmail.com>
2025-03-13 18:38:36 +0900

    add: Modelの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;   
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -9,6 +10,9 @@ class TodoController extends Controller
     
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
     
