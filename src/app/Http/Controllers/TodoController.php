62eb223c20a3a0a80d23e21aec7f324454c39fb9
gaku1130 <gk.99prd3o@gmail.com>
2025-03-14 18:50:58 +0900

    add: 新規作成画面の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,8 +14,12 @@ class TodoController extends Controller
         $todos = $todo->all();
         // $todos = todosテーブルの内容が全て入っている
         
-        
         return view('todo.index', ['todos' => $todos]); 
     }
     
+
+    public function create()
+    {
+        return view('todo.create');
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/todo', 'TodoController@index');
+Route::get('/todo/create', 'TodoController@create');
