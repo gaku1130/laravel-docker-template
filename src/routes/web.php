ce22d1c7ea9ca0dcf58f68ce7066e885ffadebb8
gaku1130 <gk.99prd3o@gmail.com>
2025-03-17 15:38:35 +0900

    add: storeメソッド

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,5 +12,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create')->name('todo.create');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
+Route::get('/todo/craete', 'TodoController@create')->name('todo.create');
+Route::post('/todo', 'TodoController@store')->name('todo.store');
